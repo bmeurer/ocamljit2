0bd1e14363a979c2b34403b41b8403301264c8b7
Benedikt Meurer <benedikt.meurer@googlemail.com>
2010-10-19 19:56:02 +0200

    Check for sigsetjmp. Fix visibility attribute in asm.
    Non-OS X systems have .hidden <name> instead of .private_extern <name>
    for GNU as.
    
    Properly determine the size of sigjmp_buf in configure and check for
    sigsetjmp() and __sigsetjmp(), as sigsetjmp() is just a macro in
    glibc.

---
+++ config/auto-aux/sigjmpbuf.c
@@ -0,0 +1,21 @@
+/***********************************************************************/
+/*                                                                     */
+/*                           Objective Caml                            */
+/*                                                                     */
+/*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         */
+/*                                                                     */
+/*  Copyright 1997 Institut National de Recherche en Informatique et   */
+/*  en Automatique.  All rights reserved.  This file is distributed    */
+/*  under the terms of the GNU Library General Public License, with    */
+/*  the special exception on linking described in file ../../LICENSE.  */
+/*                                                                     */
+/***********************************************************************/
+
+#include <setjmp.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+  printf("%d\n", sizeof(sigjmp_buf));
+  return 0;
+}
